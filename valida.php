f7e63564f72f78f7c363878b7e426b82454f771e
ingridm <ingrid_murielem@hotmail.com>
2014-05-30 23:15:00 -0300

    login

--- valida.php
+++ valida.php
@@ -1,50 +1,33 @@
-<?php
+<?
+include db.php;
 
-// Verifica se houve POST e se o usuário ou a senha é(são) vazio(s)
+global $login;
+global $senha1;
+$login= $_POST['login'];
+$senha1 = $_POST['senha1'];
+//faz a confirmação de nome e senha no db
+$logar = mysql_query("SELECT * FROM pessoa WHERE login='$login' AND senha='$senha1' AND usuario_Nivel='1'") or die("erro ao selecionar");
+/*aqui depois de verificado redirecionamos a pagina secreta(caso nome e senha estarem corretos) ou senha
+e apelido não conferem caso tais estiverem errados. Repare que há uma rotina para o valor inserido em senha não seja nulo.
 
-if (!empty($_POST) AND (empty($_POST['login']) OR empty($_POST['senha']))) {
-  header("Location: index.html"); 
-  exit;
+obs: Aonde esta escrito paginasecreta.php é aonde vc deve colocar a página para onde o script ira redirecionar*/
+if (strlen($senha1)< 1)
 
-mysql_connect('localhost', 'root', '') or trigger_error(mysql_error());
-mysql_select_db('pessoa') or trigger_error(mysql_error());
+echo "Senha ou apelido não confere tente denovo";
+session_start("login");
+//session_destroy();
 
-$login = mysql_real_escape_string($_POST['login']);
-$senha = mysql_real_escape_string($_POST['senha']);
+elseif (mysql_num_rows($logar)>0 ){
+setcookie("senha", "$senha1");
+header("location: menu.php");
 
-
-//$sql = "SELECT `id`, `nome`, `nivel` FROM `usuarios` WHERE (`usuario` = '". $login ."') AND (`senha` = '". sha1($senha) ."') AND (`ativo` = 1) LIMIT 1";
-
-$sql =" SELECT `nome`, `rg`, `email`, `veiculo`, `senha`, `id_Usuario`, `login`, `usuario_Nivel`, `cpf`, `status`, `moradia`, `foto` FROM `pessoa` WHERE (`login` = `".$login."`) AND (`senha` = `". sha1($senha) ."`) AND (`status` =1) LIMIT 1";
-$query = mysql_query($sql);
-
-if (mysql_num_rows($query) != 1) {
-   // Mensagem de erro quando os dados são inválidos e/ou o usuário não foi encontrado
-
-    echo "Login inválido!"; exit;
 } else {
+ echo "
+Senha ou apelido não conferem
+<a href=\"secAcess.php\">tente de novo</a>
 
-    // Salva os dados encontados na variável $resultado
-
-    $resultado = mysql_fetch_assoc($query);
-	// Se a sessão não existir, inicia uma
-    if (!isset($_SESSION)) session_start();
-    // Salva os dados encontrados na sessão
-
-    $_SESSION['id'] = $resultado['id'];
-
-    $_SESSION['login'] = $resultado['login'];
-
-    $_SESSION['usuario_Nivel'] = $resultado['usuario_Nivel'];
-
- 
-
-    // Redireciona o visitante
-
-   header("Location: restrita.php");
-   exit;
-
-}
+";
 }
 
-?>
+
+?
